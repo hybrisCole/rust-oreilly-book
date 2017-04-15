da2934eed264e7b4be0fb081a3ee1109f4fb3308
Alberto Cole <acpii2005@gmail.com>
2017-04-14 18:26:33 -0600

    mandelbrot exercise

---
+++ mandelbrot/target/debug/mandelbrot-5bb52b3bfd7048bc.d
@@ -0,0 +1,1 @@
+/Users/albertocole/Desktop/rust-gentle-intro/mandelbrot/target/debug/mandelbrot-5bb52b3bfd7048bc: /Users/albertocole/Desktop/rust-gentle-intro/mandelbrot/src/main.rs
---
+++ mandelbrot/target/release/mandelbrot.d
@@ -0,0 +1,1 @@
+/Users/albertocole/Desktop/rust-gentle-intro/mandelbrot/target/release/mandelbrot: /Users/albertocole/Desktop/rust-gentle-intro/mandelbrot/src/main.rs
