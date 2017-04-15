f838d5aa9824d77f4aa60c01f2ebe0fa76465240
Alberto Cole <acpii2005@gmail.com>
2017-04-14 18:29:05 -0600

    deleting target files, duh

--- mandelbrot/target/debug/mandelbrot-5bb52b3bfd7048bc.d
+++
@@ -1,1 +0,0 @@
-/Users/albertocole/Desktop/rust-gentle-intro/mandelbrot/target/debug/mandelbrot-5bb52b3bfd7048bc: /Users/albertocole/Desktop/rust-gentle-intro/mandelbrot/src/main.rs
--- mandelbrot/target/release/mandelbrot.d
+++
@@ -1,1 +0,0 @@
-/Users/albertocole/Desktop/rust-gentle-intro/mandelbrot/target/release/mandelbrot: /Users/albertocole/Desktop/rust-gentle-intro/mandelbrot/src/main.rs
